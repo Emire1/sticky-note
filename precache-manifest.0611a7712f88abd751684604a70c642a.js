167ae7dc5823cc751a875b4479ecc59b6f25c9f2
Emmanuel Mireku <Emmanuelmireku15@gmail.com>
2019-01-16 22:15:14 -0500

    Updates

---
+++ precache-manifest.0611a7712f88abd751684604a70c642a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d14d5eaabfd77178aaef",
+    "url": "/sticky-note/static/js/runtime~main.d14d5eaa.js"
+  },
+  {
+    "revision": "918301d722822f34c4a2",
+    "url": "/sticky-note/static/js/main.918301d7.chunk.js"
+  },
+  {
+    "revision": "80ef99108d0273b6a7e9",
+    "url": "/sticky-note/static/js/1.80ef9910.chunk.js"
+  },
+  {
+    "revision": "918301d722822f34c4a2",
+    "url": "/sticky-note/static/css/main.b878ff9f.chunk.css"
+  },
+  {
+    "revision": "80ef99108d0273b6a7e9",
+    "url": "/sticky-note/static/css/1.29404776.chunk.css"
+  },
+  {
+    "revision": "07df21844e885558a9d7fe65cee87bb7",
+    "url": "/sticky-note/index.html"
+  }
+];
